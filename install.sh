473c511f88d4b2e45c7629ed4cde58488920d406
Jan-Markus Langer <hallo@janmarkuslanger.de>
2018-12-04 21:20:21 +0100

    add packages atom homebrew

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+# https://mths.be/macos
+# https://github.com/mzdr/macOS
 
 # Keep-alive: update existing `sudo` time stamp until `.macos` has finished
-# https://mths.be/macos
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Colors
@@ -13,19 +14,37 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 # enter a new step
 headline() {
+    echo "$bold"
     echo "----------------------------"
     echo "                            "
-    echo "$bold  $1                   "
+    echo "      $1                    "
     echo "                            "
     echo "----------------------------"
     echo "$reset"
+    echo ""
+    echo ""
+    echo ""
 }
+
+run() {
+    eval $@
+}
+
+headline "Setting up your dev environment"
 
 # Disable the sound effects on boot
 sudo nvram SystemAudioVolume=" "
 
-headline "Hello World"
 
+# Install homebrew
+headline "Install homebrew"
+run '/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
+run brew update
+
+# Install atom
+run brew cask install atom
+
+# Install atom packages
 for package in $(<$DIR/.atom/packages); do
-    echo "$package"
+    run apm install $package
 done
